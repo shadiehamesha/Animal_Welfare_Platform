89e480a847458893504e76cf88c507bbd95219b2
10952447 - Shadie H Mudannayaka <132426805+shadiehamesha@users.noreply.github.com>
2026-03-03 10:15:44 +0530

    update the homepage to add footer

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../components/navigation";
+import Footer from "../components/footer";
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
+            <div className="min-h-screen"></div>
+            <Footer />
         </>
     );
 }
