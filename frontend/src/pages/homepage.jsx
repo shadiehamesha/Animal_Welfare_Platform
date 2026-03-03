f420bb3efdb87beac144da52d8388c6654676c6f
10952447 - Shadie H Mudannayaka <132426805+shadiehamesha@users.noreply.github.com>
2026-03-03 11:03:01 +0530

    updated homepage to include services, hero, banner text and search components

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../components/navigation";
 import Footer from "../components/footer";
+import Hero from "../components/homepage/hero";
+import BannerText from "../components/homepage/bannerText";
+import Services from "../components/homepage/services";
+import Search from "../components/homepage/search";
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
-            <div className="min-h-screen"></div>
+            <Hero />
+            <BannerText />
+            <Services />
+            <Search />
             <Footer />
         </>
     );
