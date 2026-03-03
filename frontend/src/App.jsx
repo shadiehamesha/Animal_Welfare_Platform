a296939152f0266c1d912b8cd199e3d71b1ef05b
10952447 - Shadie H Mudannayaka <132426805+shadiehamesha@users.noreply.github.com>
2026-03-03 11:36:13 +0530

    created paths to new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage.jsx';
+import About from './pages/about.jsx';
+import ContactUs from './pages/contactus.jsx';
+import PrivacyPolicy from './pages/privacypolicy.jsx'
+import Login from './pages/login.jsx';
+import SignUp from './pages/signup.jsx';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/privacy" element={<PrivacyPolicy />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
   );
 }
