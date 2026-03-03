56188b0853b8d507ce8c1c3a7621ca86de412a6d
10952447 - Shadie H Mudannayaka <132426805+shadiehamesha@users.noreply.github.com>
2026-03-04 00:39:49 +0530

    added cta, goals, hero, history, stats, team, values and vison components to the about page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Navbar from "../components/navigation";
 import Footer from "../components/footer";
+import AboutHero from "../components/about/hero";
+import VisionMission from "../components/about/visionandmission";
+import AboutValues from "../components/about/values";
+import AboutStats from "../components/about/stats";
+import AboutHistory from "../components/about/history"
+import AboutTeam from "../components/about/team";
+import AboutGoals from "../components/about/goals";
+import AboutCTA from "../components/about/cta";
 
 const About = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            
-            <main className="flex-grow flex flex-col items-center justify-center bg-[#f9fdfc] py-20 px-4 text-center">
-               <h1 className="text-3xl md:text-4xl font-bold text-slate-900 mb-4">
-                   About Us
-               </h1>
-               <p className="text-slate-500 text-lg max-w-2xl">
-                   This page is currently under construction. Check back soon!
-               </p>
-            </main>
-
+            <AboutHero />
+            <VisionMission />
+            <AboutValues />
+            <AboutStats />
+            <AboutHistory />
+            <AboutTeam />
+            <AboutGoals />
+            <AboutCTA />
             <Footer />
         </div>
     );
